77187afc461dd546a2558242e236dab7d258ba53
Alex Javier Bertrand <alex.bertrand13@gmail.com>
2015-11-29 19:32:07 -0800

    Change to launchpad interface to go value to row/col (instead of the other way around)

--- launchpad_interface.v
+++ launchpad_interface.v
@@ -1,31 +1,99 @@
 module launchpad_interface(
-	input [1:0] row,
-	input [1:0] col,
-	output [3:0] output_val
+	input [3:0] val,	
+	output [1:0] row,
+	output [1:0] col
 );
 
-	reg [3:0] out;
-	assign output_val = out;
+	reg [1:0] x,y;
+	assign row = x;
+	assign row = y;
 	
 	always @(*) begin
 		case({row, col})
-			4'b0000: out = 4'h1;
-			4'b0001: out = 4'h2;
-			4'b0010: out = 4'h3;
-			4'b0011: out = 4'hA;
-			4'b0100: out = 4'h4;
-			4'b0101: out = 4'h5;
-			4'b0110: out = 4'h6;
-			4'b0111: out = 4'hB;
-			4'b1000: out = 4'h7;
-			4'b1001: out = 4'h8;
-			4'b1010: out = 4'h9;
-			4'b1011: out = 4'hC;
-			4'b1100: out = 4'h0;
-			4'b1101: out = 4'hF;
-			4'b1110: out = 4'hE;
-			4'b1111: out = 4'hD;
-			default: out = 4'h0;
+			4'b0000: begin
+				// 0
+				x = 2'd0;
+				y = 2'd3;
+			end
+			4'b0001: begin
+				// 1
+				x = 2'd0;
+				y = 2'd0;
+			end
+			4'b0010: begin
+				// 2
+				x = 2'd1;
+				y = 2'd0;
+			end
+			4'b0011: begin
+				// 3
+				x = 2'd2;
+				y = 2'd0;
+			end
+			4'b0100: begin
+				// 4
+				x = 2'd0;
+				y = 2'd1;
+			end
+			4'b0101: begin
+				// 5
+				x = 2'd1;
+				y = 2'd1;
+			end
+			4'b0110: begin
+				// 6
+				x = 2'd2;
+				y = 2'd1;
+			end
+			4'b0111: begin
+				// 7	
+				x = 2'd0;
+				y = 2'd2;
+			end
+			4'b1000: begin
+				// 8
+				x = 2'd1;
+				y = 2'd1;
+			end
+			4'b1001: begin
+				// 9
+				x = 2'd2;
+				y = 2'd2;
+			end
+			4'b1010: begin
+				// A
+				x = 2'd3;
+				y = 2'd0;
+			end
+			4'b1011: begin
+				// B
+				x = 2'd3;
+				y = 2'd1;
+			end
+			4'b1100: begin
+				// C
+				x = 2'd3;
+				y = 2'd2;
+			end
+			4'b1101: begin
+				// D
+				x = 2'd3;
+				x = 2'd3;
+			end
+			4'b1110: begin
+				// E
+				x = 2'd2;
+				y = 2'd3;
+			end
+			4'b1111: begin
+				// F
+				x = 2'd1;
+				y = 2'd3;
+			end
+			default: begin
+				x = 2'd0;
+				y = 2'd0;
+			end
 		endcase
 	end
 
